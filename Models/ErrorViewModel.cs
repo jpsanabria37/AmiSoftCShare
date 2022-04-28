3aa0a1ea888b6dc07f461c37a7d3184f998c4903
jpsanabria37 <jpsanabria37@misena.edu.co>
2022-04-27 20:07:31 -0500

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AmiSoftCShare.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
